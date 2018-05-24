83d2134b02897f8620a5ec818127eb6614e18b4a
Maksim Soldatjonok <maksold@gmail.com>
2018-05-24 20:00:27 +0300

    Fixed migration script issue with incorrect table names

--- app/code/community/Mailigen/Synchronizer/sql/mailigen_synchronizer_setup/mysql4-upgrade-1.2.9-1.3.0.php
+++ app/code/community/Mailigen/Synchronizer/sql/mailigen_synchronizer_setup/mysql4-upgrade-1.2.9-1.3.0.php
@@ -13,14 +13,15 @@ $installer->startSetup();
 /**
  * Add 'store_id' column to 'mailigen_synchronizer/customer' table
  */
-$tableName = $installer->getTable('mailigen_synchronizer/customer');
-$installer->getConnection()->addColumn($tableName, 'store_id', 'SMALLINT(5) UNSIGNED DEFAULT NULL AFTER `website_id`');
+$mailigenTableName = $installer->getTable('mailigen_synchronizer/customer');
+$customerTableName = $installer->getTable('customer/entity');
+$installer->getConnection()->addColumn($mailigenTableName, 'store_id', 'SMALLINT(5) UNSIGNED DEFAULT NULL AFTER `website_id`');
 
 
 /*
  * Update 'mailigen_synchronizer_customer.store_id' field data from 'customer_entity table
  */
-$sql = "UPDATE mailigen_synchronizer_customer ms_customer, (SELECT entity_id, store_id FROM customer_entity) customer
+$sql = "UPDATE {$mailigenTableName} ms_customer, (SELECT entity_id, store_id FROM {$customerTableName}) customer
 SET ms_customer.store_id = customer.store_id
 WHERE ms_customer.id = customer.entity_id";
 
