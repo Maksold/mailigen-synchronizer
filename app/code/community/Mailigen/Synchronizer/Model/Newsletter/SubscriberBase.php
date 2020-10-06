4dbea28bd3ced6b38b179a2220e597b9b343ac91
Maksim Soldatenok <maksold@gmail.com>
2020-10-06 11:32:44 +0300

    Fix rewrite conflict with MW_Onestepcheckout module

--- app/code/community/Mailigen/Synchronizer/Model/Newsletter/SubscriberBase.php
+++ app/code/community/Mailigen/Synchronizer/Model/Newsletter/SubscriberBase.php
@@ -4,6 +4,10 @@ if (Mage::helper('core')->isModuleEnabled('Ebizmarts_MailChimp')
     && class_exists('Ebizmarts_MailChimp_Model_Subscriber')
 ) {
     class_alias('Ebizmarts_MailChimp_Model_Subscriber', 'Mailigen_Synchronizer_Model_Newsletter_SubscriberBase');
+} elseif (Mage::helper('core')->isModuleEnabled('MW_Onestepcheckout')
+    && class_exists('MW_Onestepcheckout_Model_Subscriber')
+) {
+    class_alias('MW_Onestepcheckout_Model_Subscriber', 'Mailigen_Synchronizer_Model_Newsletter_SubscriberBase');
 } else {
     class Mailigen_Synchronizer_Model_Newsletter_SubscriberBase extends Mage_Newsletter_Model_Subscriber
     {
